c2e2e32a07715c1ea0e897dc88c0f26882bded1a
DmitryLyapin <dmitry.lyapin.2502@mail.ru>
2026-06-08 23:32:01 +0300

    добавление периодических celery тасков удаления токенов и напоминания о дедлайнах тасков

--- app/celery_app.py
+++ app/celery_app.py
@@ -1,6 +1,19 @@
 from celery import Celery
 from app.core.config import settings
+from celery.schedules import crontab
 
 celery_app = Celery("app", broker=settings.redis.url, backend=settings.redis.url)
 celery_app.autodiscover_tasks(["app"])
+
+celery_app.conf.beat_schedule = {
+    "cleanup-revoked-tokens": {
+        "task": "app.tasks.cleanup_revoked_tokens",
+        "schedule": crontab(hour=0, minute=0),
+    },
+    "check-deadline-tasks": {
+        "task": "app.tasks.check_deadline_tasks",
+        "schedule": crontab(minute=0),
+    },
+}
+
 celery_app.conf.update(task_serializer="json", result_serializer="json")
--- app/tasks.py
+++ app/tasks.py
@@ -1,5 +1,43 @@
 from .celery_app import celery_app
+import asyncio
+from datetime import datetime, timezone, timedelta
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy import delete, select
+from app.core.config import settings
+from app.models.token import Token
+from app.models.task import Task
 
 @celery_app.task
 def send_welcome_email(email: str):
     print(f"Sending welcome email to {email}")
+
+async def cleanup_revoked_tokens_async_function():
+    engine = create_async_engine(settings.database.database_url)
+    session = async_sessionmaker(bind=engine)
+    async with session() as session:
+        await session.execute(delete(Token)
+                              .where(Token.revoked_at != None,
+                                     Token.revoked_at < datetime.now(timezone.utc) - timedelta(days=30)))
+        await session.commit()
+    await engine.dispose()
+
+async def check_deadline_tasks_async_function():
+    engine = create_async_engine(settings.database.database_url)
+    session = async_sessionmaker(bind=engine)
+    async with session() as session:
+        result = await session.execute(select(Task)
+                                       .where(Task.status != "done",
+                                              Task.due_date < datetime.now(timezone.utc) + timedelta(days=1)))
+        tasks = result.scalars().all()
+        for task in tasks:
+            print(f"Task {task.title} deadline is tomorrow!")
+    await engine.dispose()
+
+@celery_app.task
+def cleanup_revoked_tokens():
+    asyncio.run(cleanup_revoked_tokens_async_function())
+
+@celery_app.task
+def check_deadline_tasks():
+    asyncio.run(check_deadline_tasks_async_function())
+
